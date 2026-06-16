7e5995ae4c5009f61313660dff4f488c11f3484f
Script'Enjøyer <apexdevweb@gmail.com>
2026-06-17 01:29:02 +0200

    Drone market

--- app/config/DataLink.php
+++ app/config/DataLink.php
@@ -2,4 +2,5 @@
 class DataLink
 {
   public const HOME_VID_DRONE = "public/assets/videos/drone_vid_papper.mp4";
+  public const HOME_IMG_WIREFRAME = "public/assets/images/drone1Final.png";
 }
--- app/views/layouts/home.php
+++ app/views/layouts/home.php
@@ -1,4 +1,5 @@
 <section class="home-container">
-<h2 class="home-container__title"><?= DataText::NAV_HOME ?></h2>
-  <video src="<?= DataLink::HOME_VID_DRONE ?>" autoplay loop muted></video>
+  <h2 class="home-container__title"><?= DataText::NAV_HOME ?></h2>
+  <div class="home-container__droneModel" data-us-project="PgUfX1gdCLd39jRT3zJz"></div>
+  <img src="<?= DataLink::HOME_IMG_WIREFRAME ?>" class="home-container__droneWireframe" alt="drnImg">
 </section>
