e28cdaeebd2c1acc10bceffe6b708de3fdf6ae79
Script'Enjøyer <apexdevweb@gmail.com>
2026-06-17 11:53:14 +0200

    Drone market

--- app/views/partials/_nav.php
+++ app/views/partials/_nav.php
@@ -1,7 +1,7 @@
 <nav class="globale-nav">
   <ul class="globale-nav__subctnr">
-    <li class="nav-el"><a href="index.php?page=<?= Encryptor::encrypt('Home') ?>"><?= DataText::NAV_HOME ?></a></li>
-    <li class="nav-el"><a href="index.php?page=<?= Encryptor::encrypt('Drones') ?>"><?= DataText::NAV_DRONE_MENU ?></a></li>
-    <li class="nav-el"><a href="index.php?page=<?= Encryptor::encrypt('Custom') ?>"><?= DataText::NAV_CUSTOM_MENU ?></a></li>
+    <li class="nav-el"><a href="index.php?page=<?= Encryptor::encrypt('Home') ?>" class="nav-el__link"><?= DataText::NAV_HOME ?></a></li>
+    <li class="nav-el"><a href="index.php?page=<?= Encryptor::encrypt('Drones') ?>" class="nav-el__link"><?= DataText::NAV_DRONE_MENU ?></a></li>
+    <li class="nav-el"><a href="index.php?page=<?= Encryptor::encrypt('Custom') ?>" class="nav-el__link"><?= DataText::NAV_CUSTOM_MENU ?></a></li>
   </ul>
 </nav>
