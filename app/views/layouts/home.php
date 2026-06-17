339562acff82bb5b334d5e3f196e0465bab1e505
Script'Enjøyer <apexdevweb@gmail.com>
2026-06-17 11:03:05 +0200

    Drone market

--- app/views/layouts/home.php
+++ app/views/layouts/home.php
@@ -1,5 +1,7 @@
 <section class="home-container">
   <h2 class="home-container__title"><?= DataText::NAV_HOME ?></h2>
-  <div class="home-container__droneModel" data-us-project="PgUfX1gdCLd39jRT3zJz"></div>
-  <img src="<?= DataLink::HOME_IMG_WIREFRAME ?>" class="home-container__droneWireframe" alt="drnImg">
+  <div class="home-container__wrapper">
+    <div class="home-container__droneModel" data-us-project="PgUfX1gdCLd39jRT3zJz"></div>
+    <img src="<?= DataLink::HOME_IMG_WIREFRAME ?>" class="home-container__droneWireframe" alt="drnImg">
+  </div>
 </section>
